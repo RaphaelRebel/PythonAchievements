a8a61d3fc6dfefb6948f88404b933e8a76965fa2
Raphael Rebel <raphaelrebel@Raphaels-MacBook-Pro.local>
2020-09-11 15:27:30 +0200

    Opdracht Poem Printing

---
+++ PYTB1L2PoemPrinting /Poemprinting.py
@@ -0,0 +1,7 @@
+vers1 = 'Mijn naam is Raphael en ik woon in Maldenhof honderdnegen. Dit is mijn vers en hopenlijk valt het nog wel tegen.'
+vers2 = 'Ik haat wanneer ik niet te doen heb in mijn leven. Dat laat mij denken dat ik niks terug kan geven.'
+vers3 = 'Altijd heb ik spellen gespeeld, bord of digitaal. Ik krijg er nooit genoeg van want ik hou van een verhaal.'
+vers4 = 'Ook hou ik van progammeren. Ik weet dat ik dit nog moet leren, maar ik krijg er niet genoeg van.'
+vers5 = 'ik wil dit mijn toekomst maken, dit was altijd al mijn plan. En over mijn plannen zal ik altijd goed waken'
+
+print(vers1, vers2, vers3, vers4, vers5)
