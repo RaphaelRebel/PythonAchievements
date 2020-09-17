582e7822c9956652369614ef7510744e06a1f81e
RaphaelRebel <44580169+RaphaelRebel@users.noreply.github.com>
2020-09-17 09:54:10 +0200

    Add files via upload
    Hier is de updated version van Helloyou

---
+++ ditbenik.py
@@ -0,0 +1,192 @@
+Python 3.8.2 (v3.8.2:7b3ab5921f, Feb 24 2020, 17:52:18) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: 
+Hallo, .
+De datum en tijd is:
+2020-09-17 09:02:37.744979
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Gamen
+C: Sporten
+Kies A, B of C: C
+Juist!
+, wil jij dit progamma nog een keer doen? Type Y/N: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: raphael
+Hallo, raphael.
+De datum en tijd is:
+2020-09-17 09:09:27.482614
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Gamen
+C: Sporten
+Kies A, B of C: a
+Helaas niet, probeer het nog eens.
+raphael, wil jij dit progamma nog een keer doen? Type Y/N: n
+Dankjewel
+>>> 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: t
+Hallo, t.
+De datum en tijd is:
+2020-09-17 09:09:55.784452
+Traceback (most recent call last):
+  File "/Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py", line 58, in <module>
+    main()
+  File "/Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py", line 53, in main
+    intro()
+  File "/Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py", line 18, in intro
+    print("Welkom, %s! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen. Laten we meteen beginnen." %username)
+NameError: name 'username' is not defined
+>>> 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: h
+Hallo, h.
+De datum en tijd is:
+2020-09-17 09:10:09.931504
+Traceback (most recent call last):
+  File "/Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py", line 58, in <module>
+    main()
+  File "/Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py", line 53, in main
+    intro()
+TypeError: intro() missing 1 required positional argument: 'username'
+>>> 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: h
+Hallo, h.
+De datum en tijd is:
+2020-09-17 09:10:27.555690
+Welkom, h! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Gamen
+C: Sporten
+Kies A, B of C: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: al
+Hallo, al.
+De datum en tijd is:
+2020-09-17 09:24:14.983720
+Welkom, al! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Reizen
+C: Sporten
+Kies A, B of C: B
+Helaas niet, het antwoord is C. Ik ben altijd al een fan geweest van sporten
+W
+A: Tekenen
+B: Gamen
+C: Sporten
+Kies A, B of C: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: 
+Hallo, .
+De datum en tijd is:
+2020-09-17 09:33:01.198575
+Welkom, ! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Reizen
+C: Sporten
+Kies A, B of C: a
+Helaas niet, het antwoord is C. Ik ben altijd al een fan geweest van sporten
+Op welke school zit ik?
+A: ROC
+B: MediaCollege
+C: Montessori
+Kies A, B of C: c
+Juist! Ik was al zeker dat ik op het MediaCollege wilde blijven sinds ik in mijn tweede jaar zat.
+, wil jij dit progamma nog een keer doen? Type Y/N: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: Raphael
+Hallo, Raphael.
+De datum en tijd is:
+2020-09-17 09:34:18.864315
+Welkom, Raphael! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Reizen
+C: Sporten
+Kies A, B of C: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: Samuel
+Hallo, Samuel.
+
+De datum en tijd is:
+2020-09-17 09:36:59.739860
+Welkom, Samuel! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen over mij. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Reizen
+C: Sporten
+Kies A, B of C: C
+Juist! Ik ben altijd al een fan geweest van sporten
+Op welke school zit ik?
+A: ROC
+B: MediaCollege
+C: Montessori
+Kies A, B of C: B
+Juist! Ik was al zeker dat ik op het MediaCollege wilde blijven sinds ik in mijn tweede jaar zat.
+Waar ben ik geboren?
+A: Amsterdam
+B: Haarlem
+C: Almere
+Kies A, B of C: B
+Juist! Ik was al zeker dat ik op het MediaCollege wilde blijven sinds ik in mijn tweede jaar zat.
+Samuel, wil jij dit progamma nog een keer doen? Type Y/N: 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: sam
+Hallo, sam.
+De datum en tijd is:
+2020-09-17 09:39:06.413784
+Welkom, sam! Dit is mijn progamma, zelf gemaakt met het doel om jou drie vragen te stellen over mij. Laten we meteen beginnen.
+Wat doe ik graag als hobby?
+A: Tekenen
+B: Reizen
+C: Sporten
+Kies A, B of C: c
+Juist! Ik ben altijd al een fan geweest van sporten
+Op welke school zit ik?
+A: ROC
+B: MediaCollege
+C: Montessori
+Kies A, B of C: b
+Juist! Ik was al zeker dat ik op het MediaCollege wilde blijven sinds ik in mijn tweede jaar zat.
+Waar ben ik geboren?
+A: Amsterdam
+B: Haarlem
+C: Almere
+Kies A, B of C: b
+Juist! Ik was al zeker dat ik op het MediaCollege wilde blijven sinds ik in mijn tweede jaar zat.
+Dankjewel voor mijn progamma uit te testen!
+sam, wil jij dit progamma nog een keer doen? Type Y/N: n
+Dankjewel
+>>> 
+===== RESTART: /Users/raphaelrebel/Documents/Projects/Helloyou/Helloyou.py =====
+Hello you!, ik ben Raphael.
+Wie ben jij?
+Mijn naam is: 
