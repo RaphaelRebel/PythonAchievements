6305442c1b458a0f45902f83430fe06404043940
Raphael Rebel <raphaelrebel@Raphaels-MacBook-Pro.local>
2020-09-18 15:21:05 +0200

    Hier is mijn First Operation te vinden

---
+++ PYTB1L2MyFirstOperations /MyFirstOperations.py
@@ -0,0 +1,74 @@
+var1 = 1
+if var1 == 1:
+    print("true")
+
+else:
+    print("false")
+
+
+var2 = 1+2
+if var2 == 3:
+    print("true")
+
+else:
+    print("false")
+
+var3 = 2-1
+if var3 == 1:
+    print("true")
+
+else:
+    print("false")
+
+var4 = 2/1
+if var3 == 1:
+    print("true")
+
+else:
+    print("false")
+
+var5 = 2%1
+if var5 == 2:
+    print("true")
+
+else:
+    print("false")
+
+var5 += 4
+if var5 == 6:
+    print("true")
+
+else:
+    print("false")
+
+var5 /= 1
+if var5 == 5:
+    print("true")
+
+else:
+    print("false")
+
+var5 -= 1
+if var5 == 4:
+    print("true")
+
+else:
+    print("false")
+
+var6 = 2*6
+if var6 == 12:
+    print("true")
+
+else:
+    print("false")
+    
+var5 *= 1
+if var5 == 4:
+    print("true")
+
+else:
+    print("false")
+        
+        
+        
+    
