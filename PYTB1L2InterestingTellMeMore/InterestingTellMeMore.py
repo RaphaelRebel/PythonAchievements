97708e6b73c59f606184ccbd82a7a1adec1de870
Raphael Rebel <you@example.comraphaelrebel@live.com>
2020-09-18 18:06:35 +0200

    Hiet zijn de vragen voor mijn klasgenoten te vinden

---
+++ PYTB1L2InterestingTellMeMore/InterestingTellMeMore.py
@@ -0,0 +1,7 @@
+naam = input("Wat is je naam?: ")
+plaats = input("Waar woon je?: ")
+jaar = input("Hoe oud ben je?: ")
+
+print("Hallo, %s." %naam)
+print("Wat leuk dat je in %s woont" %plaats)
+print("Ben je al %s jaar oud?! Wat gaat de tijd toch snel" %jaar)
