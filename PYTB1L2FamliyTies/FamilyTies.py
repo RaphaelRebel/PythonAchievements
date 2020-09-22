447dde6583387e4d5919e75120cf6de154feb85e
Raphael Rebel <you@example.comraphaelrebel@live.com>
2020-09-23 01:16:23 +0200

    Nieuwe opdrachten gemaakt en een update gegeven

---
+++ PYTB1L2FamliyTies/FamilyTies.py
@@ -0,0 +1,7 @@
+name = "Raphael"
+age = 17
+afstand = float(16.6)
+place = "Maldenhof"
+txt = "Mijn naam is {}, ik ben {} jaar oud en ik woon in {}. dat is {} km van school "
+print(txt.format(name, age, place, afstand))
+
