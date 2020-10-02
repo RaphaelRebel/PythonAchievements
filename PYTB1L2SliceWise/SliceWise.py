da342d7408ae2eb1404609c10388db3f9b0437ec
Raphael Rebel <you@example.comraphaelrebel@live.com>
2020-10-02 13:59:36 +0200

    Hier is mijn SliceWise opdracht te vinden

---
+++ PYTB1L2SliceWise/SliceWise.py
@@ -0,0 +1,26 @@
+tekst1 = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+voornaam1 = tekst1[37:41]
+voornaam2 = tekst1[43:48]
+voornaam3 = tekst1[61:66]
+
+print(voornaam1, voornaam2, voornaam3)
+
+tekst2 = 'SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ...'
+vakken1 = tekst2[15:21]
+vakken2 = tekst2[23:26]
+vakken3 = tekst2[28:48]
+vakken4 = tekst2[52:71]
+
+print(vakken1, vakken2, vakken3, vakken4)
+
+tekst3 = 'Wat is hier het laatste woord?'
+woord1 = tekst3[24:30]
+
+print(woord1)
+
+tekst4 = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+aantal1 = tekst4[4:8]
+aantal2 = tekst4[28:33]
+
+print(aantal1, aantal2)
+
---
+++ PYTB1L2StutterSimulator/StutterSimulator.py
@@ -0,0 +1,4 @@
+print("Hello you!")
+stutter = input("Type hier iets: ")
+
+print(stutter[0:7]"...")
