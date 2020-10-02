9af83ea51f0d524c696e62151e809c0c9dd4b06a
Raphael Rebel <you@example.comraphaelrebel@live.com>
2020-10-02 14:24:45 +0200

    InterestingTellMeMore gefixt

--- PYTB1L2InterestingTellMeMore/InterestingTellMeMore.py
+++ PYTB1L2InterestingTellMeMore/InterestingTellMeMore.py
@@ -4,4 +4,4 @@ jaar = input("Hoe oud ben je?: ")
 
 print("Hallo, %s. Goed om je te zien" %naam)
 print("Wat leuk dat je in %s woont." %plaats)
-print("Goed om te weten dat je %s oud bent.")
+print("Goed om te weten dat je %s oud bent." %jaar)
